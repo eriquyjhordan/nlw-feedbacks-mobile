21cf85c948cd3ea7717036f895251429d261a1f1
Eriquy Jhordan <eriquy@gmail.com>
2022-05-05 17:25:20 -0300

    feat: create widget component

--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,5 +1,29 @@
 import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { theme } from './../../theme/index';
 
 export const styles = StyleSheet.create({
-  container: {},
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: theme.colors.brand,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 16,
+    bottom: getBottomSpace() + 16,
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: theme.colors.surface_primary,
+    paddingBottom: getBottomSpace() + 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  indicator: {
+    backgroundColor: theme.colors.text_primary,
+    width: 56,
+  },
 });
